65bfad5103e16990839c81c6c4a3b3dc5c0639a7
darragh <kruptein@gmail.com>
2017-03-31 12:39:24 +0200

    Get rid of stupid print

--- api/meta.py
+++ api/meta.py
@@ -3,7 +3,6 @@ class CommandRegisterType(type):
     A metaclass that allows the registering of commands using a simple decorator (api.decorators.command).
     """
     def __init__(cls, name, bases, attrs):
-        print()
         for key, val in attrs.items():
             try:
                 names, format = getattr(val, 'command')
