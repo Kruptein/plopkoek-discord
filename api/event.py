78043afceba7428c4596559b87600663eb27c39e
Darragh Van Tichelen <info@darragh.dev>
2020-08-30 15:40:28 +0200

    Default to sequence id 0

--- api/event.py
+++ api/event.py
@@ -4,7 +4,13 @@ from enum import IntEnum
 
 import logging
 
-from api.db import update_user, update_guild, update_channel, remove_guild, remove_channel
+from api.db import (
+    update_user,
+    update_guild,
+    update_channel,
+    remove_guild,
+    remove_channel,
+)
 from api.utils import get_logger, set_value
 
 module = sys.modules[__name__]
@@ -14,9 +20,9 @@ def __to_classname(event_name: str):
     return "".join(part.capitalize() for part in event_name.split("_"))
 
 
-def get_event(json_data) -> 'Event':
-    if json_data['op'] == 0 and hasattr(module, __to_classname(json_data['t'])):
-        return getattr(module, __to_classname(json_data['t']))(json_data)
+def get_event(json_data) -> "Event":
+    if json_data["op"] == 0 and hasattr(module, __to_classname(json_data["t"])):
+        return getattr(module, __to_classname(json_data["t"]))(json_data)
     return Event(json_data)
 
 
@@ -37,20 +43,20 @@ class GatewayOP(IntEnum):
 
 class Event:
     def __init__(self, data):
-        self._s = data['s']
-        self._t = data['t']
-        self._op = data['op']
+        self._s = data["s"]
+        self._t = data["t"]
+        self._op = data["op"]
         self.raw_data = data
 
         try:
-            for var in data['d']:
-                setattr(self, var, data['d'][var])
+            for var in data["d"]:
+                setattr(self, var, data["d"][var])
         except:
             pass
 
         self.is_dispatch = self.of(GatewayOP.DISPATCH)
 
-        set_value("main", "last_sequence_id", data['s'] if data['s'] is not None else '0')
+        set_value("main", "last_sequence_id", data["s"] if data["s"] is not None else 0)
 
     @property
     def sequence(self):
@@ -84,15 +90,15 @@ class ChannelCreate(Event):
     def __init__(self, data):
         super().__init__(data)
         # TODO FIX
-        #if self.is_private and "id" in self.recipient:
+        # if self.is_private and "id" in self.recipient:
         #    update_user(self.recipient)
-        update_channel(data['d'])
+        update_channel(data["d"])
 
 
 class ChannelUpdate(Event):
     def __init__(self, data):
         super().__init__(data)
-        update_channel(data['d'])
+        update_channel(data["d"])
 
 
 class ChannelDelete(Event):
@@ -106,23 +112,23 @@ class ChannelDelete(Event):
 class GuildCreate(Event):
     def __init__(self, data):
         super().__init__(data)
-        update_guild(data['d'])
+        update_guild(data["d"])
 
 
 class GuildUpdate(Event):
     def __init__(self, data):
         super().__init__(data)
-        update_guild(data['d'])
+        update_guild(data["d"])
 
 
 class GuildDelete(Event):
     def __init__(self, data):
         super().__init__(data)
-        remove_guild(data['d'])
+        remove_guild(data["d"])
 
 
 class MessageCreate(Event):
     def __init__(self, data):
         super().__init__(data)
-        if "webhook_id" not in data['d']:
+        if "webhook_id" not in data["d"]:
             update_user(self.author)
